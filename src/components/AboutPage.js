d96a13f3edcb3938627ba7f7b7225b712be877db
tomatoboy <hayatarou.ay@gmail.com>
2024-04-06 00:55:31 +0900

    name changed

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import History from './History'
 import './styles/AboutPage.css'
-import profileImage from './img/face.jpg';
+import profileImage from './img/face.JPG';
 function AboutPage() {
   return (
     <div className="about">
